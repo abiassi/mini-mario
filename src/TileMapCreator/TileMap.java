6b4c1c2da40da9e7f37ca4347ad8ff27d19c849b
Abi <amorim.a.ferreira@gmail.com>
2023-05-11 11:22:12 +0100

    Reset IntelliJ config

--- src/TileMapCreator/TileMap.java
+++ src/TileMapCreator/TileMap.java
@@ -1,10 +1,18 @@
 package TileMapCreator;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
 public class TileMap {
+    public static final int PADDING = 10;
+    private int cellSize = 50;
     private Tile[][] map;
 
     public TileMap(int width, int height) {
         map = new Tile[width][height];
+    }
+
+    public void init() {
+        Rectangle field = new Rectangle(PADDING, PADDING, getWidth() * cellSize, getHeight() * cellSize);
+        field.draw();
     }
 
     public void setTile(int x, int y, Tile tile) {
@@ -22,6 +30,10 @@ public class TileMap {
     public int getHeight() {
         return map[0].length;
     }
+    public int getCellSize() {
+        return cellSize;
+    }
+
 
 
     // Need to render, update, etc
